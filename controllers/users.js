cbb8d8256967cfd76bfb8d7ddfac88a4b919f0bb
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-07-06 12:45:34 -0400

    added users new view and rendered to page

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/new", (req, res) => {
-  res.send("new");
+  res.render("users/new.ejs");
 });
 
 module.exports = router;
