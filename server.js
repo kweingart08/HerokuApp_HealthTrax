993370e5b7d51eb5341392a45abd330c38d07053
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-07-06 11:51:54 -0400

    installing mongoose

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+// heroku URL: https://thawing-reef-79440.herokuapp.com/
+// can also type open heroku
+
+// ===== DEPENDENCIES =====
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// **rename database later to name of project
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/app_dev"
 
 app.get("/", (req, res)=>{
   res.send("this works");
@@ -10,4 +17,11 @@ app.listen(port, ()=>{
   console.log("-----------------------------");
   console.log("I'm listening on port: ", port);
   console.log("-----------------------------");
+});
+
+mongoose.connect(mongoUri);
+mongoose.connection.on("open", () => {
+  console.log("======================");
+  console.log("connected to mongoose!");
+  console.log("======================");
 });
