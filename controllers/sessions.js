505e3f77c24a927351001139c3709cac99051710
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-07-06 14:27:27 -0400

    doctor page working

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users.js");
+const Doctor = require("../models/doctors.js");
 
+// === when click the log in button on the front home page/index, direct to sessions/new and show the new sessions ejs file ===
 router.get("/new", (req, res) => {
   res.render("sessions/new.ejs");
 });
 
-
+// === during log in, when submit, find the user or send wrong password ===
 router.post("/", (req, res) => {
   User.findOne({username: req.body.username}, (err, foundUser) => {
     if(req.body.password == foundUser.password){
@@ -18,16 +20,24 @@ router.post("/", (req, res) => {
   });
 });
 
+// ===== AFTER LOG IN - DIRECT TO /USER AND SHOW USERINDEX.EJS =====
 router.get("/user", (req, res) => {
   res.render("sessions/userIndex.ejs", {
     currentUser: req.session.currentUser
   });
 });
 
+// ===== LOG OUT OF SESSION / DESTROY SESSION =====
 router.delete("/", (req, res) => {
   req.session.destroy(() => {
     res.redirect("/");
   });
 });
+
+router.get("/doctor", (req, res) => {
+  res.send("at doctor page to add doctor - this page should have an index with form to post")
+});
+
+
 
 module.exports = router;
