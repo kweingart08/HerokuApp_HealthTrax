e6064a1cfdf3ff72df3dfec044d2c8a4e9436e75
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-07-06 13:04:39 -0400

    create sessions controller and new page and require. register and log in working

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/users.js");
+
+router.get("/new", (req, res) => {
+  res.render("sessions/new.ejs");
+});
+
+router.post("/", (req, res) => {
+  User.findOne({username: req.body.username}, (err, foundUser) => {
+    if(req.body.password == foundUser.password){
+      res.send("logged in");
+    } else {
+      res.send("wrong password");
+    }
+  });
+});
 
 module.exports = router;
--- models/users.js
+++ models/users.js
@@ -8,4 +8,4 @@ const userSchema = Schema({
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = "User";
+module.exports = User;
