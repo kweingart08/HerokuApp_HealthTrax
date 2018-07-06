78e0fe921a59edc5275373fc1697af10ae8e83b8
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-07-06 14:19:42 -0400

    added doctor model

---
+++ models/doctors.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const doctorSchema = Schema({
+  name: String,
+  address: String,
+  phone: String,
+  last_visit: Date,
+  next_visit: Date,
+  note: String
+});
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+module.exports = Doctor;
