2eb3e43adb9b39e92f4ad3c1058621f80a181acf
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-07-06 11:27:21 -0400

    port for heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
   res.send("this works");
 });
 
 app.listen(port, ()=>{
+  console.log("-----------------------------");
   console.log("I'm listening on port: ", port);
+  console.log("-----------------------------");
 });
