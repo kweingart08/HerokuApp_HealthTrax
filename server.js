f9c750e21b540d52f5efc60e4eb27d3b3f9ab8c0
Katelynn Weingart <kweingart@Andrews-iMac.attlocal.net>
2018-07-11 08:35:18 -0400

    added a four-oh-four style page and index page for errors

--- server.js
+++ server.js
@@ -1,49 +1,65 @@
-// heroku URL: https://thawing-reef-79440.herokuapp.com/
-// can also type open heroku
-
-// ===== DEPENDENCIES =====
+// ---------------------------------
+// DEPENDENCIES
+// ---------------------------------
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
-// **rename database later to name of project
-const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/app_dev';
 const session = require("express-session");
 const methodOverride = require("method-override");
 
-// ===== MIDDLEWARE =====
+// ---------------------------------
+// PORT (set up for hosting w/ heroku)
+// ---------------------------------
+const port = process.env.PORT || 3000;
+
+// ---------------------------------
+// DATABASE (set up for hosting w/ heroku w/ mLab add on)
+// ---------------------------------
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/app_dev';
+
+// ---------------------------------
+// MIDDLEWARE
+// ---------------------------------
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
-
 app.use(session({
   secret: "feedmeseymour",
   resave: false,
   saveUninitialized: false
 }));
 
-// ===== CONTROLLERS =====
+// ---------------------------------
+// CONTROLLERS
+// ---------------------------------
 const usersController = require("./controllers/users.js");
 app.use("/users", usersController);
 
 const sessionsController = require("./controllers/sessions.js");
 app.use("/sessions", sessionsController);
 
-
-// ===== INDEX ROUTE / ROOT PAGE =====
+// ---------------------------------
+// ROUTES
+// ---------------------------------
+// localhost:3000 - this will render the main index page/home page
 app.get("/", (req, res)=>{
   res.render("index.ejs");
 });
 
+// 404 errors! - this will catch any route that doesn't exist
+app.get("*", (req, res) => {
+  res.render("four-oh-four/index.ejs");
+});
 
-// ===== APP LISTENER AND MONGOOSE CONNECTIONS =====
+// ---------------------------------
+// LISTENER & MONGOOSE CONNECTION
+// ---------------------------------
 app.listen(port, ()=>{
   console.log("-----------------------------");
   console.log("I'm listening on port: ", port);
   console.log("-----------------------------");
 });
-
 mongoose.connect(mongoUri, {useNewUrlParser: true});
 mongoose.connection.on("open", () => {
   console.log("connected to mongoose!");
